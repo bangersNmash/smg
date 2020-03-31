134d93194c05e0bc605e29acc22f845e4e0a63b2
alois9866 <5354247+alois9866@users.noreply.github.com>
2020-03-31 21:55:06 +0300

    Add basic logic (#2)
    * Add basic logic
    
    * Update test.yml

--- smg.py
+++ smg.py
@@ -1,7 +1,24 @@
+import pygame
+
+import properties
+
 
 def main():
-    pass
+    pygame.init()
+    pygame.display.set_mode((properties.width, properties.height))
+
+    while True:
+        cont = handle_event(pygame.event.wait())
+        if not cont:
+            break
+
+
+def handle_event(e) -> bool:
+    print(e)
+    if e.type == pygame.QUIT:
+        return False
+    return True
+
 
 if __name__ == '__main__':
     main()
-
--- test_main.py
+++ test_main.py
@@ -1,11 +1,20 @@
 import unittest
 
+import pygame.event
+
 import smg
 
+
 class TestMain(unittest.TestCase):
-    def test_main(self):
-        smg.main()
+
+    def test_handle_event_quit(self):
+        quit_event = pygame.event.Event(pygame.QUIT, dict={})
+        assert not smg.handle_event(quit_event)
+
+    def test_handle_event_not_quit(self):
+        not_quit_event = pygame.event.Event(pygame.K_w, dict={})
+        assert smg.handle_event(not_quit_event)
+
 
 if __name__ == '__main__':
     unittest.main()
-
