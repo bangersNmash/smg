784710dfafd0e4f02e242c4b145f035c607edf6b
alois9866 <alois9866@gmail.com>
2020-04-11 04:01:48 +0300

    Add simple grid drawing and event handling

--- smg.py
+++ smg.py
@@ -1,23 +1,53 @@
+import sys
+from math import sqrt
+
 import pygame
 
+import grid
 import properties
 
 
 def main():
     pygame.init()
-    pygame.display.set_mode((properties.width, properties.height))
+    screen = pygame.display.set_mode((properties.width, properties.height))
+    map = grid.Grid(properties.width, properties.height, properties.default_size)
 
     while True:
-        cont = handle_event(pygame.event.wait())
-        if not cont:
-            break
+        draw(screen, map)
+        action = handle_event(pygame.event.wait())
+        action(map)
 
 
-def handle_event(e) -> bool:
-    print(e)
+def draw(screen, map):
+    screen.fill((255, 255, 255))
+    for hex in map.hexes():
+        pygame.draw.aalines(screen, (0, 0, 0), True, hex.vertices())
+    pygame.display.update()
+
+
+def handle_event(e):
+    if e.type == pygame.MOUSEBUTTONDOWN:
+        return handle_click(*e.pos)
+
     if e.type == pygame.QUIT:
-        return False
-    return True
+        return finish
+    return id
+
+
+def handle_click(x, y):
+    def f(map):
+        size = map.get_size()
+        print(x, y, grid.pixel_to_hex(x - size, y - size * sqrt(3) / 2, properties.default_size))
+
+    return f
+
+
+def id(_):
+    pass
+
+
+def finish(_):
+    sys.exit(0)
 
 
 if __name__ == '__main__':
--- test_main.py
+++ test_main.py
@@ -4,10 +4,10 @@ import smg
 
 
 def test_handle_event_quit():
-    quit_event = pygame.event.Event(pygame.QUIT, dict={})
-    assert not smg.handle_event(quit_event)
+    quit_event = smg.handle_event(pygame.event.Event(pygame.QUIT, dict={}))
+    assert quit_event == smg.finish
 
 
 def test_handle_event_not_quit():
-    not_quit_event = pygame.event.Event(pygame.K_w, dict={})
-    assert smg.handle_event(not_quit_event)
+    not_quit_event = smg.handle_event(pygame.event.Event(pygame.K_w, dict={}))
+    assert not_quit_event != smg.finish
