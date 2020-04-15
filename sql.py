177cce9a5a60ab2a8d945c01296906d0344d6fdf
max101101 <suhov85@mail.ru>
2020-04-16 01:19:41 +0300

    Implement session rounds
    	Update session table.
    	Introduced new table rounds and its DB interface methods.
    	DB commit moved from database.py to server.py
    	to support transactions.
            Introduced new api method make_move.

--- sql.py
+++ sql.py
@@ -11,12 +11,30 @@ CREATE_USERS = """CREATE TABLE IF NOT EXISTS users(
 CREATE_SESSIONS = """CREATE TABLE IF NOT EXISTS sessions(
                       uuid VARCHAR PRIMARY KEY,
                       users VARCHAR NOT NULL,
+                      players INT NOT NULL,
                       payload VARCHAR NOT NULL,
                       state VARCHAR NOT NULL,
+                      round INT NOT NULL,
                       ts INT NOT NULL)"""
+
+CREATE_ROUNDS = """CREATE TABLE IF NOT EXISTS rounds(
+                    uuid VARCHAR,
+                    round INT,
+                    user_moves VARCHAR NOT NULL,
+                    PRIMARY KEY(uuid, round))"""
 
 SELECT_USER = "SELECT * FROM users WHERE username=?"
 INSERT_USER = "INSERT INTO users VALUES (?, ?)"
 SELECT_SESSION = "SELECT * FROM sessions WHERE uuid=?"
-INSERT_SESSION = "INSERT INTO sessions VALUES (?,?,?,?,?)"
-UPDATE_SESSION = "UPDATE sessions SET users=?, payload=?, state=?, ts=? WHERE uuid=?"
+INSERT_SESSION = "INSERT INTO sessions VALUES (?,?,?,?,?,?,?)"
+UPDATE_SESSION = """UPDATE sessions SET
+                     users=?,
+                     players=?,
+                     payload=?,
+                     state=?,
+                     round=?,
+                     ts=?
+                     WHERE uuid=?"""
+SELECT_ROUND = "SELECT * FROM rounds WHERE uuid=?, round=?"
+INSERT_ROUND = "INSERT INTO rounds VALUES (?,?,?)"
+UPDATE_ROUND = "UPDATE rounds SET user_moves=? WHERE uuid=?, round=?"
